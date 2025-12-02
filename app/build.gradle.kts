9e24c5c66b81d28a9688f45495a7bb1b06cdce37
grgttdln <georgettedalen@gmail.com>
2025-12-02 12:44:22 +0800

    fix: gradle for connection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.app"
+        applicationId = "com.example.kusho"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
